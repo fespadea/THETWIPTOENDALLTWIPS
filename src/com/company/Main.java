4c63f662aad6668c43cd253afd9bd60f998394fd
fspadea <fs279@pvsd.org>
2017-04-12 14:28:35 -0400

    cough cough

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner raw = new Scanner(new File("rawDataU6.txt"));
+    }
+}
