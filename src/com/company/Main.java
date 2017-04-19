d1eb405f897aaf4a368404c65f91078143ce1f5b
fspadea <fs279@pvsd.org>
2017-04-19 14:29:00 -0400

    fghs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,21 @@ package com.company;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         Scanner raw = new Scanner(new File("rawDataU6.txt"));
+        ArrayList<Person> lty = new ArrayList<>();
+        raw.nextLine();
+        while(raw.hasNext()){
+            Person now = new Person(raw.nextInt());
+            String current = raw.next();
+            while(current.contains()){
+
+            }
+        }
     }
 }
---
+++ src/com/company/Person.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by fs279 on 4/19/17.
+ */
+public class Person {
+    public Person(int p){
+        person = p;
+    }
+    public int person;
+}
