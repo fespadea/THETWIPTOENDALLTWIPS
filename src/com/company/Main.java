ae1b5429ca058694b7d9fe7a3b41b34df0b35a5c
Fernando Spadea <fespadearocks@gmail.com>
2017-05-12 18:45:21 -0400

    I am actually done this time

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -28,14 +28,12 @@ public class Main {
                 pi = Double.parseDouble(listOfLines.get(i).next());
                 if(pi != lty.get(index).person) {
                     Person now = new Person(pi);
-                    System.out.println("pi: " + pi);
                     lty.add(now);
                     index++;
                 }
             }
             catch (Exception e){
                 Person now = new Person(1.0);
-                System.out.println("pi: " + 1.0);
                 lty.add(now);
                 index++;
             }
@@ -43,7 +41,6 @@ public class Main {
             double cur;
             int x = 0;
             current = listOfLines.get(i).next();
-            System.out.println("current: " + current);
             Scanner s;
             int add1 = 0;
             while(current.contains("+") && !current.contains("Setup loop to step through wordlist")) {
@@ -53,9 +50,7 @@ public class Main {
                     if(add1 == 1) {
                         try {
                             cur = Double.parseDouble(p);
-                            System.out.println("count: " + x++);
                             lty.get(index).score1 += cur;
-                            System.out.println("score: " + lty.get(index).score1);
                         } catch (Exception e) {
                         }
                         add1 = 0;
@@ -65,30 +60,23 @@ public class Main {
                     }
                 }
                 current = listOfLines.get(i).next();
-                System.out.println("current: " + current);
             }
             int d = 0;
             while(!current.contains("+") && d == 0) {
                 try {
-                    System.out.println("currentsyntaxbeforeerror: " + current);
                     lty.get(index).score1 -= Double.parseDouble(current) / 4.0;
-                    System.out.println("scoresyntax: " + lty.get(index).score1);
                     d++;
                 } catch (Exception e) {
                     current = listOfLines.get(i).next();
-                    System.out.println("currentsyntax: " + current);
                     s = new Scanner(current);
                 }
             }
-            System.out.println("score: " + lty.get(index).score1);
             double syn = 0;
             while(!current.contains("+") && d == 1) {
                 try{
                     current = listOfLines.get(i).next();
-                    System.out.println("currentsyntax2: " + current);
                     s = new Scanner(current);
                     syn = s.nextDouble();
-                    System.out.println("syn: " + syn);
                     d++;
                 } catch (Exception e){
                 }
@@ -101,9 +89,7 @@ public class Main {
                     if(add == 1) {
                         try {
                             cur = Double.parseDouble(p);
-                            System.out.println("count2: " + x++);
                             lty.get(index).score2 += cur;
-                            System.out.println("score2: " + lty.get(index).score2);
                         } catch (Exception e) {
                         }
                         add = 0;
@@ -113,20 +99,16 @@ public class Main {
                     }
                 }
                 current = listOfLines.get(i).next();
-                System.out.println("current2: " + current);
             }
             if(d != 2){
                 try {
-                    System.out.println("score2beforesyntax: " + lty.get(index).score2);
                     lty.get(index).score2 -= Double.parseDouble(current) / 4;
-                    System.out.println("score2syntax: " + lty.get(index).score2);
                 }
                 catch (Exception e){
                 }
             }
             else {
                 lty.get(index).score2 -= syn/4;
-                System.out.println("score2: " + lty.get(index).score2);
             }
         }
         lty.get(11).score1 *= 2;
@@ -134,16 +116,12 @@ public class Main {
         lty.get(20).score1 *= 2;
         lty.get(24).score1 *= 2;
         lty.get(25).score1 *= 2;
+        lty.get(0).score2 *= 2;
         lty.get(11).score2 *= 2;
         lty.get(14).score2 *= 2;
         lty.get(20).score2 *= 2;
         lty.get(24).score2 *= 2;
         lty.get(25).score2 *= 2;
-        for (int i = 0; i < 28; i++) {
-            System.out.println("pi: " + lty.get(i).person);
-            System.out.println("score1: " + lty.get(i).score1 / 2);
-            System.out.println("score2: " + lty.get(i).score2 / 2);
-        }
         for(Person p: lty){
             Scanner check = new Scanner(new File("names.txt"));
             for (int i = 0; i < 28; i++) {
@@ -154,7 +132,7 @@ public class Main {
                         d = check.nextDouble();
                     }
                     catch (Exception e){
-                        maybe += check.next();
+                        maybe += check.next() + " ";
                     }
                 }
                 if(p.person == d){
@@ -163,9 +141,62 @@ public class Main {
             }
         }
         Collections.sort(lty);
-        System.out.println("Name\t\tSecret Number\t\tAvg Total Grade\t\tFR Q1 Grade\t\tFR Q2 Grade");
+        System.out.println("Name\t\t\t\tSecret Number\tAvg Total Grade\t\tFR Q1 Grade\t\t\tFR Q2 Grade");
         for(Person p: lty){
-            System.out.println(p.name + "\t\t" + p.person + "\t\t" + p.score2+p.score1+"/19" + "\t\t" + p.score1 + "\t\t" + p.score2);
+            if((p.score1/2+"").toString().length() < 4) {
+                if (((p.score2 + p.score1) / 2 + "/19").toString().length() < 8) {
+                    if (p.name.length() > 13) {
+                        System.out.println(p.name + "\t" + (int) p.person + "\t\t\t\t" + (p.score2 + p.score1) / 2 + "/19" + "\t\t\t\t" + p.score1 / 2 + "\t\t\t\t\t" + p.score2 / 2);
+                    } else if (p.name.length() > 11) {
+                        System.out.println(p.name + "\t\t" + (int) p.person + "\t\t\t\t" + (p.score2 + p.score1) / 2 + "/19" + "\t\t\t\t" + p.score1 / 2 + "\t\t\t\t\t" + p.score2 / 2);
+                    } else if (p.name.length() > 7) {
+                        System.out.println(p.name + "\t\t\t" + (int) p.person + "\t\t\t\t" + (p.score2 + p.score1) / 2 + "/19" + "\t\t\t\t" + p.score1 / 2 + "\t\t\t\t\t" + p.score2 / 2);
+                    } else if (p.name.length() < 4) {
+                        System.out.println(p.name + "\t\t\t\t\t" + (int) p.person + "\t\t\t\t" + (p.score2 + p.score1) / 2 + "/19" + "\t\t\t\t" + p.score1 / 2 + "\t\t\t\t\t" + p.score2 / 2);
+                    } else {
+                        System.out.println(p.name + "\t\t\t\t" + (int) p.person + "\t\t\t\t" + (p.score2 + p.score1) / 2 + "/19" + "\t\t\t\t" + p.score1 / 2 + "\t\t\t\t\t" + p.score2 / 2);
+                    }
+                } else {
+                    if (p.name.length() > 13) {
+                        System.out.println(p.name + "\t" + (int) p.person + "\t\t\t\t" + (p.score2 + p.score1) / 2 + "/19" + "\t\t\t" + p.score1 / 2 + "\t\t\t\t\t" + p.score2 / 2);
+                    } else if (p.name.length() > 11) {
+                        System.out.println(p.name + "\t\t" + (int) p.person + "\t\t\t\t" + (p.score2 + p.score1) / 2 + "/19" + "\t\t\t" + p.score1 / 2 + "\t\t\t\t\t" + p.score2 / 2);
+                    } else if (p.name.length() > 7) {
+                        System.out.println(p.name + "\t\t\t" + (int) p.person + "\t\t\t\t" + (p.score2 + p.score1) / 2 + "/19" + "\t\t\t" + p.score1 / 2 + "\t\t\t\t\t" + p.score2 / 2);
+                    } else if (p.name.length() < 4) {
+                        System.out.println(p.name + "\t\t\t\t\t" + (int) p.person + "\t\t\t\t" + (p.score2 + p.score1) / 2 + "/19" + "\t\t\t" + p.score1 / 2 + "\t\t\t\t\t" + p.score2 / 2);
+                    } else {
+                        System.out.println(p.name + "\t\t\t\t" + (int) p.person + "\t\t\t\t" + (p.score2 + p.score1) / 2 + "/19" + "\t\t\t" + p.score1 / 2 + "\t\t\t\t\t" + p.score2 / 2);
+                    }
+                }
+            }
+            else{
+                if (((p.score2 + p.score1) / 2 + "/19").toString().length() < 8) {
+                    if (p.name.length() > 13) {
+                        System.out.println(p.name + "\t" + (int) p.person + "\t\t\t\t" + (p.score2 + p.score1) / 2 + "/19" + "\t\t\t\t" + p.score1 / 2 + "\t\t\t\t" + p.score2 / 2);
+                    } else if (p.name.length() > 11) {
+                        System.out.println(p.name + "\t\t" + (int) p.person + "\t\t\t\t" + (p.score2 + p.score1) / 2 + "/19" + "\t\t\t\t" + p.score1 / 2 + "\t\t\t\t" + p.score2 / 2);
+                    } else if (p.name.length() > 7) {
+                        System.out.println(p.name + "\t\t\t" + (int) p.person + "\t\t\t\t" + (p.score2 + p.score1) / 2 + "/19" + "\t\t\t\t" + p.score1 / 2 + "\t\t\t\t" + p.score2 / 2);
+                    } else if (p.name.length() < 4) {
+                        System.out.println(p.name + "\t\t\t\t\t" + (int) p.person + "\t\t\t\t" + (p.score2 + p.score1) / 2 + "/19" + "\t\t\t\t" + p.score1 / 2 + "\t\t\t\t" + p.score2 / 2);
+                    } else {
+                        System.out.println(p.name + "\t\t\t\t" + (int) p.person + "\t\t\t\t" + (p.score2 + p.score1) / 2 + "/19" + "\t\t\t\t" + p.score1 / 2 + "\t\t\t\t" + p.score2 / 2);
+                    }
+                } else {
+                    if (p.name.length() > 13) {
+                        System.out.println(p.name + "\t" + (int) p.person + "\t\t\t\t" + (p.score2 + p.score1) / 2 + "/19" + "\t\t\t" + p.score1 / 2 + "\t\t\t\t" + p.score2 / 2);
+                    } else if (p.name.length() > 11) {
+                        System.out.println(p.name + "\t\t" + (int) p.person + "\t\t\t\t" + (p.score2 + p.score1) / 2 + "/19" + "\t\t\t" + p.score1 / 2 + "\t\t\t\t" + p.score2 / 2);
+                    } else if (p.name.length() > 7) {
+                        System.out.println(p.name + "\t\t\t" + (int) p.person + "\t\t\t\t" + (p.score2 + p.score1) / 2 + "/19" + "\t\t\t" + p.score1 / 2 + "\t\t\t\t" + p.score2 / 2);
+                    } else if (p.name.length() < 4) {
+                        System.out.println(p.name + "\t\t\t\t\t" + (int) p.person + "\t\t\t\t" + (p.score2 + p.score1) / 2 + "/19" + "\t\t\t" + p.score1 / 2 + "\t\t\t\t" + p.score2 / 2);
+                    } else {
+                        System.out.println(p.name + "\t\t\t\t" + (int) p.person + "\t\t\t\t" + (p.score2 + p.score1) / 2 + "/19" + "\t\t\t" + p.score1 / 2 + "\t\t\t\t" + p.score2 / 2);
+                    }
+                }
+            }
         }
     }
 }
